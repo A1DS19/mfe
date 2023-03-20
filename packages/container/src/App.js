3cadb21a8531bb02b0a7ed0a54a021b27456a46f
Jose Padilla <jp06171@gmail.com>
2023-03-20 14:17:11 -0600

    Randomize prod classNames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+// This is to avoid class name collisions between the apps and the container app
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <React.Fragment>
+      <StylesProvider generateClassName={generateClassName}>
         <Header />
         <MarketingApp />
-      </React.Fragment>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+// This is to avoid class name collisions between the marketing app and the container app
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
 export default () => {
   return (
-    <StylesProvider injectFirst>
+    <StylesProvider generateClassName={generateClassName}>
       <Router>
         <Switch>
           <Route path='/pricing' component={Pricing} />
